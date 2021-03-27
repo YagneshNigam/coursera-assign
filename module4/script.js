13c99c88304e86fb2936885df5d96480914a7438
YagneshNigam <80233055+YagneshNigam@users.noreply.github.com>
2021-03-27 21:50:53 +0530

    Update script.js

--- module4/script.js
+++ module4/script.js
@@ -1,6 +1,7 @@
+
 (function () {
 
-var names = ["Rishi", "James", "Linus", "Albert", "Micheal", "William", "Rahul", "Adarsh", "Beth", "Amit"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
